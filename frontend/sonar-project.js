b89659392a0fa0c30c3652050a5f6a1d34e5793c
fakeita <fatimakeite05@gmail.com>
2025-11-06 01:13:08 +0000

    again

---
+++ frontend/sonar-project.js
@@ -0,0 +1,15 @@
+const sonarqubeScanner = require("sonarqube-scanner");
+
+sonarqubeScanner(
+    {
+        serverUrl: "http://localhost:9000",
+        token: process.env.SONAR_TOKEN,
+        options: {
+            "sonar.projectKey": "app-frontend",
+            "sonar.projectName": "app-frontend",
+            "sonar.sources": "src",
+            "sonar.exclusions": "**/*.spec.ts",
+        },
+    },
+    () => process.exit()
+);
