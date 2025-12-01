688dbd397f380d0f315a3324cf3a988b0885826d
fakeita <fatimakeite05@gmail.com>
2025-12-01 11:09:56 +0000

    test

---
+++ user-service/src/test/java/com/cgl/userservice/config/DataInitializerTest.java
@@ -0,0 +1,32 @@
+package com.cgl.userservice.config;
+
+import com.cgl.userservice.data.entities.User;
+import com.cgl.userservice.data.enums.Role;
+import com.cgl.userservice.data.repositories.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class DataInitializerTest {
+    @Mock
+    private UserRepository userRepository;
+    @Mock
+    private PasswordEncoder passwordEncoder;
+    @InjectMocks
+    private DataInitializer dataInitializer;
+    @Value("${application.default.user.password}")
+    private String defaultPassword;
+    @Test
+    void initializeData() {
+        String password = defaultPassword;
+       assertEquals("tima04", password);
+
+    }
+}
